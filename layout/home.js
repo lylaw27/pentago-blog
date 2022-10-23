e88e8e87ca65cc45159a07f0b20b7e2711e873ba
lylaw27 <lylaw27@gmail.com>
2022-10-24 00:29:35 +0800

    hi

--- layout/home.js
+++ layout/home.js
@@ -23,6 +23,16 @@ export default function Home({blogs, sidebar, pagination}){
             <link rel="icon" href="/favicon.ico"/>
         </Head>
       <Script src="https://kit.fontawesome.com/dbb3bd5296.js" crossorigin="anonymous"/>
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{__html: `(window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-TJ8JNZMR1Y');)`,}}/>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-TJ8JNZMR1Y"></Script>
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{__html: `(!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '730992154174691');fbq('track', 'PageView');)`}}>
+    </Script>
         <Header/>
       <div className="overlap">
       <div className="background">
