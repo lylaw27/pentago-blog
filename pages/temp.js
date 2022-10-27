1a8ea2d1a45331cad195054b4c939a5862cb57b9
lylaw27 <lylaw27@gmail.com>
2022-10-27 16:27:11 +0800

    hi

--- pages/temp.js
+++ pages/temp.js
@@ -11,14 +11,16 @@ export default function Temp({article}){
 
 export async function getStaticProps(){
     const blogs = await Dbconnect('blogs');
-    const urlconverter = (title) =>{
+    // const data = await blogs.updateMany({},{$set :{'url': urlconverter($title)}})
+    blogs.find().forEach((item)=>{
+        let string1 = item.title;
         let regex1 = /[\u007b-\u007e\u005B-\u0060\u003A-\u0040\u0020-\u002F\u2000-\u206f\uff00-\uffef\u3000-\u303f\s]+/g;
-        let string2 = title.replace(regex1,' ').trim()
-        console.log(string2)
+        let string2 = string1.replace(regex1,' ').trim()
         let regex2 = /\s/g;
-        return string2.replace(regex2,'-');
+        let result = string2.replace(regex2,'-');
+        blogs.updateOne({_id: item._id},{'$set':{'url': result}})
     }
-    const data = await blogs.updateMany({},{$set :{'url': urlconverter($title)}})
+    )
     return{
         props:{
             article: 'done'
