330ca6a42e6861681c463428d5e0dd9f19930d78
lylaw27 <lylaw27@gmail.com>
2022-10-27 16:38:27 +0800

    hi

--- pages/temp.js
+++ pages/temp.js
@@ -12,7 +12,7 @@ export default function Temp({article}){
 export async function getStaticProps(){
     const blogs = await Dbconnect('blogs');
     // const data = await blogs.updateMany({},{$set :{'url': urlconverter($title)}})
-    blogs.find().forEach((item)=>{
+    await blogs.find().forEach((item)=>{
         let string1 = item.title;
         let regex1 = /[\u007b-\u007e\u005B-\u0060\u003A-\u0040\u0020-\u002F\u2000-\u206f\uff00-\uffef\u3000-\u303f\s]+/g;
         let string2 = string1.replace(regex1,' ').trim()
