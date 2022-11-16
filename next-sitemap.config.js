1a46d3842842a0c907e9e10d7ae5e87d64d7ccc9
lylaw27 <lylaw27@gmail.com>
2022-11-16 18:49:47 +0800

    deploy

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,15 @@
+var urlencode = require('urlencode')
+
 module.exports = {
     siteUrl: process.env.AUTH0_BASE_URL || 'https://researcherp.com',
     generateRobotsTxt: true, // (optional),
     exclude: ['/admin/*'],
+    transform: async (config, path) => {
+        return {
+            loc: urlencode(path.substring(1)), // => this will be exported as http(s)://<config.siteUrl>/<path>
+            changefreq: config.changefreq,
+            priority: config.priority,
+        }
+    }
     // ...other options
   }
